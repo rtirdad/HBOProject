a003f64717df8c5636ec231d183be3062a0e713e
rtirdad <94053847+rtirdad@users.noreply.github.com>
2023-03-14 17:29:10 +0100

    onderzoek page completed

--- onderzoek.php
+++ onderzoek.php
@@ -1,1 +1,97 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <b><a class="navbar-brand fs-1 " href="#">
+                    <img src="img/Logo.png" alt="" width="120"> nformation
+                </a>
+            </b>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="about.php">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Information</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row mt-4">
+            <div class="col-md-12 text-center">
+                <h1>hallo allemaal dit is onze <b>hoofdvraag en deelvraag</b> pagina</h1>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-md-12 text-center">
+               <p>In hoeverre kunnen ICT-studenten die overstappen van het MBO naar HBO zich beter kunnen voorbereiden met benodigde vaardigheden en kennis?
+
+                   1e deel vraag: Welke vaardigheden van de MBO kunnen de ICT-studenten gebruiken voor een voorsprong op het HBO?
+                   2e deel vraag: Wat voor kennis kunnen de ICT-studenten van het MBO gebruiken op het HBO?
+                   3e deel vraag: Wat zijn de belangrijkste uitdagingen en obstakels waar ICT-studenten mee te maken krijgen bij de overstap naar het HBO?
+
+                   Bij de keuzedeel HBO-voorbereiding is onze project om meer te leren over wat er in hbo zit en ons te voorbereiden voor later als we ernaar toe wilt. Op 4 februari waren twee teamleden van onze groep naar de open dag van De Haagse hogeschool gegaan. We hadden als een groep 14 interview vragen bedacht en bij de open dag, hadden de twee teamleden de student en docenten geïnterviewd.
+
+                   Je zal je best afvragen welke vaardigheden van de MBO kunnen de ICT-studenten gebruiken voor een voorsprong op het HBO? Het antwoord op deze vraag licht aan de studie die de student doet. De studenten die ICT systems and devices doen die hebben een voor sprong bij de vakken zoals datacom, cybersecurity en op python (scripting), terwijl de studenten van de opleiding software development een voorsprong hebben op de vakken die zij krijgen zoals: java script,php, vuew en html css. Het ligt natuurlijk ook aan wat de student kiest om te doen op het hbo. Bijvoorbeeld als een student van systems and devices kiest om op hbo software kant te gaan. Dan zouden wij aanraden om de vakken van de opleiding software development te oefenen. Je zou je ook af kunnen vragen wat voor kennis kunnen de ICT-studenten van het MBO gebruiken op het HBO? Dit hebben wij zonet ook al een beetje beantwoord, kort gezegd licht het aan de studie die de student heeft gedaan voor hbo en wat hij gaat doen op hbo. Als een student van systems and devices cybersecurity gaat doen op het hbo dan is zijn kennis over cybersecurity die hij heeft gekregen op het mbo handig, hetzelfde geld voor studenten van software development. Als die ook verder gaan in software development dan is hun kennis over de vakken die zij al hadden gekregen op het mbo handig om al te kunnen beheersen wanneer ze naar het hbo gaan. Op het hbo word er meer team projecten gedaan dus de studenten moeten goed voorbereid om in teams te kunnen werken. Op het mbo heb je ook in het laatste jaar stage zo heb je ook ervaring kunnen opbouwen met het bedrijfsleven. De belangrijkste uitdagingen en obstakels waar de ICT-studenten rekening moeten houden zou zijn goed in een team kunnen werken en de vakken waar van zij minder van weten, zoals bijvoorbeeld als een student van de opleiding systems en devices software wilt gaan doen op het hbo dan zullen de andere scripting talen een val kuil kunnen zijn voor die studenten, voor de studenten die op het mbo software doen als die in de cybersecurity gaan dan kan dat een punt zijn waar zij aan zouden moeten werken.
+
+                   Met deze informatie zouden we kunnen zeggen dat je een voorsprong krijgt van de mbo studie die je het laaste jaar hebt gedaan en dat de studenten zich kunnen voorbereiden door op de dingen te focussen die zijn niet hebben gehad.
+               </p>
+            </div>
+        </div>
+
+
+
+
+
+    <footer class="text-center text-lg-start bg-light text-muted">
+
+        <section class="d-flex justify-content-center justify-content-lg-between p-2 border-bottom">
+            <div class="container text-center text-md-start mt-5">
+                <div class="row text-center">
+                    <div class="col-md-4">
+                        <a class="text-decoration-none text-secondary" href="about.php"><h5>About</h5></a>
+
+                    </div>
+                    <div class="col-md-4">
+                        <a class="text-decoration-none text-secondary" href="about.php"><h5>Contact</h5></a>
+
+                    </div>
+                    <div class="col-md-4">
+                        <a class="text-decoration-none text-secondary" href="about.php"><h5>Information</h5></a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <p class="text-center">@2022 HBO Voorbereiding All Rights Reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </footer>
+
+</div>
+
+
+</body>
+</html>
